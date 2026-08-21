0ffecc85d56390b65a25183cd7e6fdc4e82a5b8a
Mostafa Gheta <mostafagheta75@gmail.com>
2026-08-21 18:21:58 +0300

    Initial commit

---
+++ .config.groovy
@@ -0,0 +1,31 @@
+/*
+   JTE configuration for Spring Petclinic Application
+*/
+
+@Library('jte-templates') _
+
+pipelineConfig {
+    /* Application Pipeline Library Setup */
+    libraries {
+        merge = true
+    }
+
+    /* Template configuration */
+    template = 'app_pipeline'
+
+    /* Build/App Config */
+    app_name = "spring-petclinic"
+    sonar_project_key = "spring-petclinic-main"
+    
+    /* ECR Config */
+    ecr_registry = "130299714330.dkr.ecr.eu-central-1.amazonaws.com"
+    ecr_repo = "petclinic"
+    aws_region = "eu-central-1"
+    
+    /* GitOps Config */
+    gitops_repo = "https://github.com/mostafagheta/gitops-repo.git"
+    
+    /* Versioning Config */
+    s3_bucket = "atos-versioning-bucket"
+    s3_version_file = "versions/version.json"
+}
